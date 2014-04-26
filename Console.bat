2021b4fb833e080dff16cb26ca608a081abb8b2b
hsfzxjy <956357208@qq.com>
2014-04-26 23:57:35 +0800

    Complete models for accounts and file_upload, as well as a view for file upload.

---
+++ Console.bat
@@ -0,0 +1,95 @@
+@echo off
+color 3f 
+
+:menu
+cls
+set ch=""
+echo **************************************
+echo *   1: Pip tool                      *
+echo *   2: Open IDEs                     *
+echo *   3: Open browser and test         *
+echo *   4: Startup manage.py             *
+echo *   5: CMD                           *
+echo *   6: Mysql                         *
+echo *   q: Exit                          *
+echo **************************************
+set /p ch="Please input a choice:"
+cls
+if /i "%ch%"=="1" goto 1
+if /i "%ch%"=="2" goto 2
+if /i "%ch%"=="3" goto 3
+if /i "%ch%"=="4" goto 4
+if /i "%ch%"=="5" goto 5
+if /i "%ch%"=="6" goto 6
+if /i "%ch%"=="q" goto end
+goto menu
+
+:6
+mysql -uroot -p
+goto menu
+
+:5
+cmd
+goto menu
+
+:1
+echo Input the package name:
+set /p Name=
+cd/d "E:\Program Files\Python\Scripts"
+echo You are going to install %Name%
+pip install %Name%
+pause
+goto menu
+
+:2
+cls
+set select=""
+echo Select the IDE you need:
+echo *************************************
+echo *   1: Python Wing IDE              *
+echo *   2: Dreamweaver                  *
+echo *   3: PhotoShop                    *
+echo *   4: Notepad++                    *
+echo *   q: exit                         *
+echo *************************************
+set /p select=
+if /i "%select%"=="1" goto 21
+if /i "%select%"=="2" goto 22
+if /i "%select%"=="3" goto 23
+if /i "%select%"=="4" goto 24
+if /i "%select%"=="q" goto menu
+goto 2
+
+:21
+start "" "E:\Program Files\Wing IDE 101 4.1\bin\wing-101.exe"
+goto 2
+
+:22
+start "" "E:\Program Files\AdobeDreamweaver_cn\Dreamweaver.exe"
+goto 2
+
+:23
+start "" "E:\Program Files\Photoshop7.0\photoshop.exe"
+goto 2
+
+:24
+start "" "E:\Program Files\Notepad++\notepad++.exe"
+goto 2
+
+:3
+explorer.exe "http://localhost:8080"
+goto menu
+
+:4
+set /p choice="manage> "
+if /i "%choice%"=="exit" goto menu
+if /i "%choice%"=="cls" goto cls
+python manage.py %choice%
+echo.
+goto 4
+:cls
+cls
+goto 4
+
+:end
+@echo on
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,79 @@
+#coding=utf-8
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from common.storage import SAEStorage
+from file_upload.models import PrivateFile, PublicFile
+from annoying.fields import AutoOneToOneField
 
-# Create your models here.
+class AccountBase(object):
+	
+	display_name = ''
+	
+class Admin(AccountBase):
+
+	display_name = u'管理员'	
+		
+class Account(AccountBase, models.Model):
+	
+	display_name = models.CharField(max_length = 255)
+	
+	class Meta:
+		abstract = True
+		ordering = ['display_name']
+		
+class Person(Account):
+
+	profile = models.OneToOneField('UserProfile', unique = True, related_name = 'person_info')
+	cash = models.DecimalField(max_digits = 4, decimal_places = 2, default = 0)
+	fixed_assets = models.DecimalField(max_digits = 4, decimal_places = 2, default = 0)
+	debt_file = models.ForeignKey(PrivateFile, related_name = 'person_in_debt')
+	consumption_reports = models.ManyToManyField(PrivateFile, related_name = 'person_owned_reports')
+	
+	class Meta:
+		pass
+	
+class Company(Account):
+	
+	profile = models.OneToOneField('UserProfile', unique = True, related_name = 'company_info')
+	description = models.TextField(null = True, blank = True)
+	members = models.TextField(null = True, blank = True)
+	phone_number = models.CharField(null = True, blank = True, max_length = 11)
+	financial_reports = models.ManyToManyField(PrivateFile, related_name = 'company_owned_reports')
+	
+	class Meta:
+		pass
+		
+class UserProfile(models.Model):
+	
+	COMPANY = 'CY'
+	PERSON  = 'PN'
+	ADMIN   = 'AN'
+	
+	USER_TYPE_CHOICES = (
+			(COMPANY, 'company'),
+			(PERSON,  'person'),
+			(ADMIN,   'admin'),
+	)
+	
+	user = AutoOneToOneField(User, unique = True, related_name = 'profile')
+	user_type = models.CharField(max_length = 2, choices = USER_TYPE_CHOICES, default = PERSON)
+	
+	@property
+	def info(self):
+		if not hasattr(self, '_info'):
+			try:
+				if self.user_type == self.PERSON:
+					self._info = self.person_info
+				elif self.user_type == self.COMPANY:
+					self._info = self.company_info
+				else:
+					self._info = Admin()
+			except ObjectDoesNotExist:
+				if self.user_type == self.PERSON:
+					self._info = Person.objects.create(profile = self)
+				elif self.user_type == self.COMPANY:
+					self._info = Company.objects.create(profile = self)
+		return self._info
+	
