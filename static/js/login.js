c202a3a1f240cbce0dd1962f4578b26dcff26dbf
hfercc <137133744@qq.com>
2014-05-17 23:08:04 +0800

    5-16

--- static/js/login.js
+++ static/js/login.js
@@ -7,16 +7,41 @@ $(document).ready(
 function validate_form() {
   var uname=$("#inputUsername");
   var pword=$("#inputPassword");
-  if((uname.val().length) == 0){
-    $("#ufg").addClass("has-error");
-  }if((pword.val().length) == 0){
-    $("#pfg").addClass("has-error");
+  if(((uname.val().length) == 0) || ((pword.val().length) == 0)){
+	  add_error("empty");
   }
-};
-
-function clean_error() {
+  else
+  {
+	  var d = login();
+  }
+}
+function add_error(type) {
+	if(type=="empty") {
+	    $("#ufg").addClass("has-error");
+	    $("#pfg").addClass("has-error");
+		$("#Result").text("用户名或密码不能为空").addClass("visible");
+	}
+	else {
+	}
+}
+function clean_form() {
 	var uname=$("#inputUsername");
-   var pword=$("#inputPassword");
+    var pword=$("#inputPassword");
 	$("#ufg").removeClass("has-error");
 	$("#pfg").removeClass("has-error");
-};
+	if($("#Result").hasClass("visible")){
+	    uname.val("");
+	    pword.val("");
+	}
+	$("#Result").val("").removeClass("visible");
+}
+function login() {
+	$.ajax({
+		url : "",
+		dataType: "json",
+		data : $('#login-form').serialize(),
+		success: function(data) {
+			return data;
+		}
+	});
+}
