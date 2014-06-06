0ded57e606f4ebfa90a0ae4bf1f0c87a34c89b59
hsfzxjy <956357208@qq.com>
2014-06-06 15:12:28 +0800

    Fix conflict

--- api/urls.py
+++ api/urls.py
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-from django.conf.urls import url, patterns, include
-
-from rest_framework.routers import DefaultRouter
-from rest_framework.viewsets import ViewSetMixin
-from rest_framework.views import APIView
-
-from .core import cache
-
-extra_list = []
-router = DefaultRouter()
-for pattern, obj in cache.get_routers():
-	print pattern
-	if issubclass(obj, ViewSetMixin):
-		router.register(pattern, obj)
-	elif issubclass(obj, APIView):
-		extra_list.append(url(pattern, obj.as_view()))
-	elif callable(obj) or isinstance(obj, (str, unicode)):
-		extra_list.append(url(pattern, obj))
-				
-extra_list.append(url(r'^', include(router.urls)))
-
-urlpatterns = patterns('',
-	url(r'^auth/', include('rest_framework.urls', namespace = 'rest_framework')),
-	*extra_list
-)
-=======
 from django.conf.urls import url, patterns, include
 
 from rest_framework.routers import DefaultRouter
@@ -51,4 +24,3 @@ urlpatterns = patterns('',
 	url(r'^root/$', 'api.views.root',),
 	*extra_list
 )
->>>>>>> e126ec4767adba9bfe044ac755d159dde5f77ec9
--- api/views.py
+++ api/views.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-# Create your views here.
-=======
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 def root(request):
 	return render_to_response('root.html', {}, context_instance = RequestContext(request))
->>>>>>> e126ec4767adba9bfe044ac755d159dde5f77ec9
